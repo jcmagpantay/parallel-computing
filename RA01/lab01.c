57a1ace9119762045b816fa312323da71c15c027
jcmagpantay <jcmagpantay@up.edu.ph>
2026-04-06 13:35:02 +0800

    Research Activity 1: Serial Computation

---
+++ RA01/lab01.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+float moving_ave(int **x, int i, int j, int q) {
+	float sum = 0;
+	for (int k = i - q; k <= i - 1; k++) {
+		sum += x[k-1][j-1];
+	}
+	return sum/q;
+}
+
+float* mse_ma(int** x, int q, int n) {
+	float *p = (float*)malloc(n*sizeof(float));
+
+	float sum = 0;
+	// p_j loop
+	for (int j = 1; j <= n; j++) {
+		sum = 0;
+		for (int i = q + 1; i <= n; i++) {
+			sum += pow(x[i-1][j-1] - moving_ave(x, i, j, q), 2);
+		}
+		p[j-1] = sqrt(sum) / (n - q);
+	}
+
+	return p;
+}
+
+float max(float x, float y) {
+	return x > y ? x : y;
+}
+
+float min(float x, float y) {
+	return x < y ? x : y;
+}
+
+int main(int argc, char* argv[]) {
+	// Initialize the random seed for RNG
+	srand(time(NULL));
+
+	// Arguments checker
+	if (argc != 2) {
+		printf("Usage: ./lab01 <n>, where n > 0 and n <= 100");
+		return 1;
+	}
+
+	int n = atoi(argv[1]);
+		
+	// Student number: 09848
+	int ss = 98;
+
+	// Initialize q
+	int q = (int) max(n/2, min(ss * n/100, 3*n/4));
+
+	// Initialize p
+	float *p;
+	
+	// Create nxn matrix
+	int **matrix = (int**)malloc(n * sizeof(int*));
+
+	for (int i = 0; i < n; i++)
+		matrix[i] = (int*)malloc(n * sizeof(int));
+
+	// Populate matrix with random numbers
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			matrix[i][j] = (rand() % 100) + 1;
+	
+	clock_t start_time, end_time;
+
+	start_time = clock();
+	p = mse_ma(matrix, q, n);
+	end_time = clock();
+
+	printf("P: ");
+	for (int i = 0; i < n; i++)
+		printf("%.4f\t", p[i]);
+	printf("\n");
+
+
+	printf("Q: %d\n", q);
+	printf("N: %d\n", n);
+	double time_taken = ((double)(end_time - start_time))/CLOCKS_PER_SEC;
+	printf("Time Taken: %f sec\n", time_taken);
+}
+
+
